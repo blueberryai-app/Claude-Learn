367e4abb9fb42be91e7a50547d0e40fdb285f247
dmatar <dmatar@apple.com>
2025-11-05 11:07:34 -0800

    Initial Commit

---
+++ Blueberry Learn/Blueberry_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blueberry_LearnApp.swift
+//  Blueberry Learn
+//
+//  Created by Daniel Matar on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Blueberry_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blueberry LearnTests/Blueberry_LearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  Blueberry_LearnTests.swift
+//  Blueberry LearnTests
+//
+//  Created by Daniel Matar on 11/5/25.
+//
+
+import Testing
+@testable import Blueberry_Learn
+
+struct Blueberry_LearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
